3d5368c268e9194aca8648a6cdb967efc6c0fcff
Benjamin CHAINTREUIL <benjamin@chaintreuil.net>
2020-11-01 23:16:24 +0100

    Update Main.java

--- POO/TP3-Bis/src/tp3/v2/Main.java
+++ POO/TP3-Bis/src/tp3/v2/Main.java
@@ -12,7 +12,7 @@ import java.io.StringWriter;
  */
 public class Main {
 	// Vars
-	static String content;
+	private static String content;
 	
 	// Methods
 	
@@ -28,19 +28,25 @@ public class Main {
 	 */
 	public static String loadFile(String path) {
 	    try {
-	       File fichier = new File(path);
-	       BufferedInputStream in = new BufferedInputStream(new FileInputStream(fichier));
-	       StringWriter out = new StringWriter();
-	       int b;
-	       while ((b=in.read()) != -1)
-	           out.write(b);
-	       out.flush();
-	       out.close();
-	       in.close();
-	       return out.toString();
-	    }
-	    catch (IOException ie)
-	    {
+	       File file = new File(path);
+	       if(file.isFile()) {
+		       BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+		       StringWriter out = new StringWriter();
+		       int b;
+		       
+		       while ((b=in.read()) != -1) {
+		           out.write(b);
+		       }
+		       
+		       out.flush();
+		       out.close();
+		       in.close();
+		       return out.toString(); 
+	       } else {
+	    	   System.out.println("Error: Enter a valid file path.");
+	    	   System.exit(1);
+	       }
+	    } catch (IOException ie) {
 	         ie.printStackTrace(); 
 	    }
 	    return null;
@@ -57,9 +63,67 @@ public class Main {
 					printHelp();
 					break;
 				case "--input":
-					// TODO : args[1] is a valid path and no more args
-					content = loadFile(args[1]);
-					
+					if(args.length == 2) {
+						content = loadFile(args[1]);
+						
+						if(content == null || content.isBlank()) {
+							System.out.println("Error: File is empty.");
+							System.exit(1);
+						} else {
+							SVGParser input = new SVGParser(content);
+							
+							System.out.println("Nombre de balise : " + input.nbrTags());
+							System.out.println("----- Balises -----");
+
+							input.displayTags();
+							
+							System.out.println("----- Polygones -----");
+							Polygone[] p = input.getPolygones();
+
+							Triangle[][] triangulations = new Triangle[p.length][];
+							
+							for(int i = 0; i < p.length; i++) {
+								System.out.println("===> Polygone #" + (i + 1));
+								
+								System.out.println("Nombre de points : " + p[i].nbrPoints());
+								System.out.println("Périmètre : " + p[i].perimetre());
+								System.out.print("Barycentre : ");
+								p[i].barycentre().print();
+								
+								System.out.println("\nListe de points :");
+								for(int j = 0; j < p[i].nbrPoints(); j++) {
+									p[i].getPoint(j).print();
+								}
+								
+								System.out.println("\nTriangulation en cours...");
+								triangulations[i] = p[i].trianguler();
+								System.out.println("Triangulation terminée !");
+								System.out.println(triangulations[i].length + " triangles calculés.");
+								
+								System.out.println("\n----- Triangles du polygone #" + (i + 1) + " -----");
+								for(Triangle t: triangulations[i]) {
+									t.OA.print();
+									t.OB.print();
+									t.OC.print();
+									System.out.print("\n");
+								}
+							}
+							
+							// Génération du SVG
+							System.out.println("----- Génération du SVG -----");
+							SVGGenerator output = new SVGGenerator(content);
+							
+							for(Triangle[] triangulation: triangulations) {
+								output.addTriangulation(triangulation);
+							}
+							
+							output.export();
+							
+							System.out.println("\n----- All done ! =D -----");
+						}
+						break;
+					}
+					printHelp();
 					break;
 				default:
 					printHelp();
@@ -68,58 +132,5 @@ public class Main {
 		} else {
 			printHelp();
 		}
-		
-		if(content == null) {
-			content = loadFile("poly.svg");
-		}
-		SVGParser input = new SVGParser(content);
-		
-		System.out.println("Nombre de balise : " + input.nbrTags());
-		System.out.println("----- Balises -----");
-
-		input.displayTags();
-		
-		System.out.println("----- Polygones -----");
-		Polygone[] p = input.getPolygones();
-
-		Triangle[][] triangulations = new Triangle[p.length][];
-		
-		for(int i = 0; i < p.length; i++) {
-			System.out.println("===> Polygone #" + (i + 1));
-			
-			System.out.println("Nombre de points : " + p[i].nbrPoints());
-			System.out.println("Périmètre : " + p[i].perimetre());
-			System.out.print("Barycentre : ");
-			p[i].barycentre().print();
-
-			
-			System.out.println("\nListe de points :");
-			for(int j = 0; j < p[i].nbrPoints(); j++) {
-				p[i].getPoint(j).print();
-			}
-			
-			System.out.println("\nTriangulation en cours...");
-			triangulations[i] = p[i].trianguler();
-			System.out.println("Triangulation terminée !");
-			System.out.println(triangulations[i].length + " triangles calculés.");
-			
-			System.out.println("\n----- Triangles du polygone #" + (i+1) + " -----");
-			for(Triangle t: triangulations[i]) {
-				t.OA.print();
-				t.OB.print();
-				t.OC.print();
-				System.out.print("\n");
-			}
-		}
-		
-		// Génération du SVG
-		System.out.println("----- Génération du SVG -----");
-		SVGGenerator output = new SVGGenerator(content);
-		
-		for(Triangle[] triangulation: triangulations) {
-			output.addTriangulation(triangulation);
-		}
-		
-		output.export();
 	}
 }

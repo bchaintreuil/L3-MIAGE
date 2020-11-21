47652133eff60919b051326538d66a5395eb792a
Benjamin CHAINTREUIL <benjamin@chaintreuil.net>
2020-11-21 18:43:07 +0100

    Define ellipse & circle

--- POO/TP4/src/unice/l3miage/cpoo/tp4/Circle.java
+++ POO/TP4/src/unice/l3miage/cpoo/tp4/Circle.java
@@ -1,11 +1,22 @@
 package unice.l3miage.cpoo.tp4;
 
 public class Circle extends Shape {
-    public double aire() {
+    private Vecteur center;
+    private double radius;
 
+    public double aire() {
+        return Math.pi * Math.pow(radius, 2);
     }
 
-    public double perimetre(){
+    public Vecteur getCenter() {
+        return this.barycentre();
+    }
 
+    public Vecteur barycentre(){
+        return this.center;
+    }
+
+    public double getRadius() {
+        return this.radius;
     }
 }
--- POO/TP4/src/unice/l3miage/cpoo/tp4/Ellipse.java
+++ POO/TP4/src/unice/l3miage/cpoo/tp4/Ellipse.java
@@ -1,4 +1,27 @@
 package unice.l3miage.cpoo.tp4;
 
-public class Ellipse extends Shape{
+public class Ellipse extends Shape {
+    private Vecteur center;
+    private double radiusX;
+    private double radiusY;
+
+    public double aire() {
+        return Math.pi * radiusX * radiusY;
+    }
+
+    public Vecteur barycentre(){
+        return this.center;
+    }
+
+    public Vecteur getCenter() {
+        return this.barycentre();
+    }
+
+    public double getRadiusX() {
+        return this.radiusX;
+    }
+
+    public double getRadiusY() {
+        return this.radiusY;
+    }
 }

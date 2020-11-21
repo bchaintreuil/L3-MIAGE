4f5281f5b96037cabda5f243befeadad5a1f9ee1
ThomasDELMARE <thomas.delmare@etu.unice.fr>
2020-11-21 22:13:33 +0100

    Code des classes Polyline et Line

--- POO/TP4/src/unice/l3miage/cpoo/tp4/Line.java
+++ POO/TP4/src/unice/l3miage/cpoo/tp4/Line.java
@@ -1,4 +1,17 @@
 package unice.l3miage.cpoo.tp4;
 
 public class Line {
+    public Vecteur OA, OB;
+
+    /**
+     * Constructeur de la classe
+     * @param OA : Vecteur-point de départ de la ligne
+     * @param OB : Vecteur-point d'arrivée de la ligne
+     */
+
+    public Line(Vecteur OA, Vecteur OB)
+    {
+        this.OA=OA;
+        this.OB=OB;
+    }
 }

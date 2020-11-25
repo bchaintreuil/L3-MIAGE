97e9e3dd7210fb9bce74d2bcccd95e3da132e3b5
ThomasDELMARE <thomas.delmare@etu.unice.fr>
2020-11-25 19:40:32 +0100

    Update TriangleTest.java

--- POO/TP4/src/unice/l3miage/cpoo/tp4/ShapesTests/TriangleTest.java
+++ POO/TP4/src/unice/l3miage/cpoo/tp4/ShapesTests/TriangleTest.java
@@ -1,4 +1,30 @@
 package unice.l3miage.cpoo.tp4.ShapesTests;
 
+import org.junit.Test;
+import unice.l3miage.cpoo.tp4.Shape.Triangle;
+import unice.l3miage.cpoo.tp4.Vecteur;
+
+import static org.junit.Assert.assertEquals;
+
 public class TriangleTest {
+    @Test
+    public void testPerimetre() {
+        Triangle unTriangle = new Triangle(new Vecteur(0.0, 0.0), new Vecteur(1.0, 0.0),new Vecteur(0.0, 1.0));
+        assertEquals(3, unTriangle.perimètre(),0);
+    }
+
+    @Test
+    public void testAire() {
+        Triangle unTriangle = new Triangle(new Vecteur(0.0, 0.0), new Vecteur(1.0, 0.0),new Vecteur(0.0, 1.0));
+        assertEquals(0.433012701892, unTriangle.aire(),10e-2);
+    }
+
+    @Test
+    public void testBarycentre() {
+        Triangle unTriangle = new Triangle(new Vecteur(0.0, 0.0), new Vecteur(1.0, 0.0),new Vecteur(0.0, 1.0));
+        var barycentre = unTriangle.barycentre();
+
+        assertEquals(0, barycentre.get(0), 0);
+        assertEquals(0, barycentre.get(1), 0);
+    }
 }
